698a7661af907230a354af292370e3fa85f8b97f
root <root@h2582873.stratoserver.net>
2017-11-28 07:35:23 +0100

    idk bruh

--- db/migrate/20171128054506_create_mapping_table.rb
+++ db/migrate/20171128054506_create_mapping_table.rb
@@ -1,6 +1,8 @@
 class CreateMappingTable < ActiveRecord::Migration[5.1]
   def change
-    create_table :mapping_tables do |t|
+    create_table :employers_resumes, :id => false do |t|
+      t.integer :employer_id
+      t.integer :resume_id
     end
   end
 end
